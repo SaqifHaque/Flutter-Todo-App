5798f324af3c54fa1ee32e1af9c21a2ebb912596
Saqif <saqifhaque@gmail.com>
2023-03-19 23:52:55 +0600

    Working on Todo_item widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,50 +10,59 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
       body: Container(
-        padding:EdgeInsets.symmetric(
+        padding: EdgeInsets.symmetric(
             horizontal: 20,
-            vertical:15
+            vertical: 15
         ),
         child: Column(
-          children:[
-            searchBox(),
-            ListView(
-              children:[
-                Container(
-                  margin: EdgeInsets.only()
-                )
-              ]
-            )
+            children:[
+              searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                    margin: EdgeInsets.only(top:),
+                    child: Text(
+                      'All ToDos',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500
+                      ),
+                    ),
+                  ],
+              )
           ],
         ),
       ),
     );
   }
-  Widget searchBox(){
+
+  Widget searchBox() {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 15),
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20)
-        ),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20)
+      ),
       child: TextField(
         decoration: InputDecoration(
-          contentPadding: EdgeInsets.all(0),
-          prefixIcon: Icon(
-            Icons.search,
-            color: tdBlack,
-            size: 20
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(
+                Icons.search,
+                color: tdBlack,
+                size: 20
             ),
-          prefixIconConstraints: BoxConstraints(
-            maxHeight: 20,
-            minWidth: 25,
-          ),
-          border: InputBorder.none,
-          hintText: 'search ',
-          hintStyle: TextStyle(color: tdGrey)
+            prefixIconConstraints: BoxConstraints(
+              maxHeight: 20,
+              minWidth: 25,
+            ),
+            border: InputBorder.none,
+            hintText: 'search ',
+            hintStyle: TextStyle(color: tdGrey)
         ),
       ),
-    ),
+    )
+    ,
   }
 
   AppBar _buildAppBar() {
@@ -63,20 +72,20 @@ class Home extends StatelessWidget {
       title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-        Icon(
-            Icons.menu,
-            color: tdBlack,
-            size: 30
-        ),
-        Container(
-          height: 40,
-          width: 40,
-          child:ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child:Image.asset('assets/images/images.jpeg'),
-          )
-        )
-      ]),
+            Icon(
+                Icons.menu,
+                color: tdBlack,
+                size: 30
+            ),
+            Container(
+                height: 40,
+                width: 40,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/images/images.jpeg'),
+                )
+            )
+          ]),
     );
   }
 
