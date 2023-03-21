9b1ae25371b12f8a0d43f5652a4cdfb9a59cd169
Saqif <saqifhaque@gmail.com>
2023-03-21 23:18:59 +0600

    add todo textfield design

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,34 +12,76 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 15
-        ),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top:50, bottom:20),
-                    child: Text(
-                      'All ToDos',
-                      style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.w500
+      body: Stack(children: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Column(
+            children: [
+              searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 50, bottom: 20),
+                      child: Text(
+                        'All ToDos',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w500),
                       ),
                     ),
-                  ),
-                  for ( ToDo todo in todoList )
-                    ToDoItem(todo: todo),
-                ],
+                    for (ToDo todo in todoList) ToDoItem(todo: todo),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
+        Positioned(child:
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Row(children: [
+            Expanded(
+              child: Container(
+                  margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0, 0.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 0.0,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Add a new todo item',
+                        border: InputBorder.none
+                    ),
+                  )),
+            ),
+            Container(
+                margin: EdgeInsets.only(
+                  bottom: 20,
+                  right: 20,
+              ),
+              child: ElevatedButton(
+                child: Text('+', style: TextStyle(fontSize: 40)),
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: tdBlue,
+                  minimumSize: Size(60, 60),
+                  elevation: 10,
+                )
+              )
+            ),
+          ]),
+        ),
+        ),
+      ]
       ),
     );
   }
@@ -48,25 +90,18 @@ class Home extends StatelessWidget {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 15),
       decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20)
-      ),
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
       child: TextField(
         decoration: InputDecoration(
             contentPadding: EdgeInsets.all(0),
-            prefixIcon: Icon(
-                Icons.search,
-                color: tdBlack,
-                size: 20
-            ),
+            prefixIcon: Icon(Icons.search, color: tdBlack, size: 20),
             prefixIconConstraints: BoxConstraints(
               maxHeight: 20,
               minWidth: 25,
             ),
             border: InputBorder.none,
             hintText: 'search ',
-            hintStyle: TextStyle(color: tdGrey)
-        ),
+            hintStyle: TextStyle(color: tdGrey)),
       ),
     );
   }
@@ -75,24 +110,16 @@ class Home extends StatelessWidget {
     return AppBar(
       backgroundColor: tdBGColor,
       elevation: 0,
-      title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Icon(
-                Icons.menu,
-                color: tdBlack,
-                size: 30
-            ),
-            Container(
-                height: 40,
-                width: 40,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.asset('assets/images/images.jpeg'),
-                )
-            )
-          ]),
+      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Icon(Icons.menu, color: tdBlack, size: 30),
+        Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/images.jpeg'),
+            ))
+      ]),
     );
   }
-
 }
